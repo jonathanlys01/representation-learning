94f289b6986225e6ef46131eab94aaeeacbbd236
jonathanlys01 <jonathanlystahiti@gmail.com>
2024-03-23 04:53:46 +0100

    results sam. 23 mars 2024 04:53:46 CET

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@
 #python3 train.py --model conv --n_epochs 500 --name conv --dataset cifar
 #python3 test.py --model conv --name conv --dataset cifar --num_samples 10
 
-python3 train.py --model vae --n_epochs 1000 --name vae_long  --dataset cifar --batch_size 256
+python3 train.py --model vae --n_epochs 1000 --name vae_long  --dataset cifar --batch_size 512
 python3 train.py --model vae --n_epochs 100 --name vae_short --dataset cifar --batch_size 256
 
 python3 test.py --model vae --name vae_long --dataset cifar --num_samples 10
