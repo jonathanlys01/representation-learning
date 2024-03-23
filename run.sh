b2523b573c38eab9e28889c0bc02c5f4c0da7c37
jonathanlys01 <jonathanlystahiti@gmail.com>
2024-03-24 00:12:51 +0100

    results dim. 24 mars 2024 00:12:51 CET

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 # train
 
-python3 train.py --model ae --n_epochs 100 --name ae --dataset cifar --batch_size 512
-python3 train.py --model conv --n_epochs 100 --name conv --dataset cifar --batch_size 512
+#python3 train.py --model ae --n_epochs 100 --name ae --dataset cifar --batch_size 512
+#python3 train.py --model conv --n_epochs 100 --name conv --dataset cifar --batch_size 512
 
 #python3 train.py --model conv --n_epochs 500 --name conv --dataset cifar
 #python3 test.py --model conv --name conv --dataset cifar --num_samples 10
@@ -13,6 +13,9 @@ python3 train.py --model conv --n_epochs 100 --name conv --dataset cifar --batch
 
 #python3 test.py --model vae --name vae_long --dataset cifar --num_samples 10
 #python3 test.py --model vae --name vae_short --dataset cifar --num_samples 10
+
+python3 train.py --model pca --n_epochs 200 --name pca --dataset cifar -b 512
+python3 train.py --model pca --n_epochs 500 --name pca_long --dataset cifar -b 512
 
 git add .
 git commit -m "results $(date)"
